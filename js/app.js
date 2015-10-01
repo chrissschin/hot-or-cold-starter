2dce3123f43651e402951dae098f3b8e52f107e8
chrissschin <solischris92@gmail.com>
2015-10-01 00:19:54 -0700

    finished fully functional game

--- js/app.js
+++ js/app.js
@@ -11,33 +11,61 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-		//NEW game button refreshes window
+    
+		//NEW game button refreshes window (for now)
 		$(".new").click(function(){
-			location.reload();
+      //sets default text
+      $("#feedback").text("Make your Guess! 1-100")
+      //clears the list of numbers used
+			$("#guessList").empty();
+      //redoes the random num on click
+      randNum = Math.floor(Math.random() * 100) + 1;
+  		console.log(randNum);
+      //clears count and sets it back to 0
+      $("#count").empty().html("0");
+
+
 		});
 
+		// initialize count
+		var count = 0;
 		//generate a random number
 		var randNum = Math.floor(Math.random() * 100) + 1;
 		console.log(randNum);
 
-		//check to see if number is too low or too high or correct
-		$("#guessButton").click(function() {
+		$('#guessForm').on('submit', function(e) {
+			// Prevent the default behavior of a form
+			e.preventDefault();
 			//set userGuess into guessedNum
-			var guessedNum = $("#userGuess");
+			var guessedNum = $("#userGuess").val();
+			// Get absolute value of the difference
+			var difference = Math.abs(guessedNum - randNum);
 			//check to see if rand num == input
-			if ( guessedNum.val() == randNum) {
+			if ( guessedNum == randNum) {
 				//shoots congrats to html
 				$("#guessList").html("<p>Congrats you got it right!</p>")
 			}
-			else if (guessedNum.val() < randNum) {
-				$("#feedback").html("<p>too low</p>");
+			else if (difference <= 5) {
+				$("#feedback").html("<p>YOU'RE ON FIRE!</p>");
 			}
-			else if (guessedNum.val() > randNum) {
-				$("#feedback").html("<p>too high</p>");
+			else if (difference <= 20) {
+				$("#feedback").html("<p>GETTING WARM</p>");
+			}
+			else if (difference <= 50){
+				$("#feedback").html("<p>YOU'RE COLD</p>");
+			}
+			else if (difference > 51){
+				$("#feedback").html("<p>FROZEN</p>");
 			}
 			else {
-				guessedNum.val('').focus();
-
+				guessedNum.focus();
 			}
+			// increment the count
+			count++
+			$("#count").text(count);
+      $("#guessList").append(guessedNum + ",   ");
+
+
+
 		});
 });
